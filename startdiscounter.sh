9a70b2db033d127e691645f17a00e81818817018
renebisperink <rene.bisperink@kiwa.com>
2022-10-11 13:22:04 +0200

    electronicadiscounter bugfixes

--- startdiscounter.sh
+++ startdiscounter.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-sudo docker stop $(docker ps -qa)
+docker stop $(docker ps -qa)
 #docker run -d -e "NODE_ENV=kiwa" -p 3000:3000 mu71l473d/kiwa-shop:v1.0.0
-#sudo docker run -d -i -t -e "NODE_ENV=kiwa" -v $(pwd)/config/kiwa.yml:/juice-shop/config/kiwa.yml -p 3000:3000 kiwashopregistry.azurecr.io/samples/kiwashop 
-sudo docker run -d -e "NODE_ENV=demo" -v $(pwd)/config/demo.yml:/juice-shop/config/demo.yml -p 80:3000 kiwashopregistry.azurecr.io/samples/kiwashop:dev
-#sudo docker run -d -e "NODE_ENV=mozilla" -v $(pwd)/config/kiwa.yml:/juice-shop/config/kiwa.yml -p 3000:3000 kiwashopregistry.azurecr.io/samples/kiwashop:dev
-sudo docker ps -a
-
+docker run -d -e "NODE_ENV=demo" -v "$(pwd)/config/demo.yml:/juice-shop/config/demo.yml" -p 80:3000 kiwashopregistry.azurecr.io/samples/kiwashop:dev
+docker ps -a
