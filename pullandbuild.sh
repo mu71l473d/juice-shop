0bf211ab4679b274f9674834e99a9ddd90fd65c3
RB <rene.bisperink@kiwa.com>
2024-07-18 18:10:10 +0200

    colouring

--- pullandbuild.sh
+++ pullandbuild.sh
@@ -1,3 +1,11 @@
+#!/bin/bash
+
+GREEN='\033[1;32m'
+NC='\033[0m'
+
+echo -e "${GREEN}[*] Starting docker if it is not started${NC}"
 sudo systemctl start docker
+echo -e "${GREEN}[*] Pulling newest version from github${NC}"
 git pull
+echo -e "${GREEN}[*] Starting the build with tag kiwashopregistry.azurecr.io/samples/kiwashop:dev${NC}"
 sudo docker build -t kiwashopregistry.azurecr.io/samples/kiwashop:dev .
--- startshop.sh
+++ startshop.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-echo "[*] Stopping old containers. dstop.sh is not necessary anymore."
-sudo docker stop $(sudo docker ps -a | grep kiwashop | awk -F ' ' '{print $1}')
-sudo docker rm $(sudo docker ps -a | grep kiwashop | awk -F ' ' '{print $1}')
+GREEN='\033[1;32m'
+NC='\033[0m'
 
-echo "[*] Starting shop."
+echo -e "${GREEN}[*] Stopping old kiwashop old containers. dstop.sh is not necessary anymore.${NC}"
+sudo docker stop $(sudo docker ps -a | grep kiwashop | awk -F " " '{print $1}')
+echo -e "${GREEN}[*]Removing non-active images${NC}"
+sudo docker rm $(sudo docker ps -a | grep kiwashop | awk -F " " '{print $1}')
+
+echo -e "${GREEN}[*]Starting kiwashop."
 #sudo docker run -d -e "NODE_ENV=kiwa" -v $(pwd)/config/kiwa.yml:/juice-shop/config/kiwa.yml -p 80:3000 kiwashopregistry.azurecr.io/samples/kiwashop:dev
 sudo docker run -d -e "NODE_ENV=kiwa" -v $(pwd)/src/app/navbar/navbar.component.html:/juice-shop/src/app/navbar/navbar.component.html -p 80:3000 kiwashopregistry.azurecr.io/samples/kiwashop:dev
 
-echo "[*] showing the process of the container"
+echo -e "${GREEN}[*]showing the process of the container${NC}"
 sudo docker ps -a | grep kiwashop
 
