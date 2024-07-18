6ddf7d835f0d93555f884b5cd59dbea2a6c0fcbc
RB <rene.bisperink@kiwa.com>
2024-07-18 17:39:10 +0200

    kiwa modules

--- dstop.sh
+++ dstop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker stop $(docker ps -a -q)
-docker rm $(docker ps -a -q)
+sudo docker stop $(sudo docker ps -a | grep kiwashop | awk -F ' ' '{print $1}')
+sudo docker rm $(sudo docker ps -a | grep kiwashop | awk -F ' ' '{print $1}')
 
--- startshop.sh
+++ startshop.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+echo "[*] Stopping old containers. dstop.sh is not necessary anymore."
+sudo docker stop $(sudo docker ps -a | grep kiwashop | awk -F ' ' '{print $1}')
+sudo docker rm $(sudo docker ps -a | grep kiwashop | awk -F ' ' '{print $1}')
 
-sudo docker stop $(docker ps -qa)
-#docker run -d -e "NODE_ENV=kiwa" -p 3000:3000 mu71l473d/kiwa-shop:v1.0.0
-#sudo docker run -d -i -t -e "NODE_ENV=kiwa" -v $(pwd)/config/kiwa.yml:/juice-shop/config/kiwa.yml -p 3000:3000 kiwashopregistry.azurecr.io/samples/kiwashop 
-sudo docker run -d -e "NODE_ENV=kiwa" -v $(pwd)/config/kiwa.yml:/juice-shop/config/kiwa.yml -p 80:3000 kiwashopregistry.azurecr.io/samples/kiwashop:dev
-#sudo docker run -d -e "NODE_ENV=mozilla" -v $(pwd)/config/kiwa.yml:/juice-shop/config/kiwa.yml -p 3000:3000 kiwashopregistry.azurecr.io/samples/kiwashop:dev
-sudo docker ps -a
+echo "[*] Starting shop."
+#sudo docker run -d -e "NODE_ENV=kiwa" -v $(pwd)/config/kiwa.yml:/juice-shop/config/kiwa.yml -p 80:3000 kiwashopregistry.azurecr.io/samples/kiwashop:dev
+sudo docker run -d -e "NODE_ENV=kiwa" -v $(pwd)/src/app/navbar/navbar.component.html:/juice-shop/src/app/navbar/navbar.component.html -p 80:3000 kiwashopregistry.azurecr.io/samples/kiwashop:dev
+
+echo "[*] showing the process of the container"
+sudo docker ps -a | grep kiwashop
 
